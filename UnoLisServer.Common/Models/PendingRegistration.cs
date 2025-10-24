21722b10aedbd43e51b9b6e4b1ad022543ab598b
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-10-24 11:39:12 -0600

    Added email and notification helpers for registration
    (IN-PROGRESS) Introduced EmailSender and NotificationSender classes to support sending verification and password reset emails. Added PendingRegistration model and CodeType enum for registration workflow. Updated project references and configuration to include SMTP settings.

---
+++ UnoLisServer.Common/Models/PendingRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoLisServer.Common.Models
+{
+    public class PendingRegistration
+    {
+        public string Nickname { get; set; }
+        public string FullName { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
