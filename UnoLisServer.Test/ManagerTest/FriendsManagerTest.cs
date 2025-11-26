97d6aafdda4e13aa81f89837937819b5b2f11920
Erick <erickmelvzlpz@gmail.com>
2025-11-26 03:42:55 -0600

    Added Tests for Friends & Deleted FriendsLogicManager

---
+++ UnoLisServer.Test/ManagerTest/FriendsManagerTest.cs
@@ -0,0 +1,305 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using UnoLisServer.Common.Exceptions;
+using UnoLisServer.Contracts.DTOs;
+using UnoLisServer.Contracts.Interfaces;
+using UnoLisServer.Data;
+using UnoLisServer.Data.Repositories;
+using UnoLisServer.Data.RepositoryInterfaces;
+using UnoLisServer.Services;
+using Xunit;
+
+namespace UnoLisServer.Test
+{
+    public class FriendsManagerTest
+    {
+        private readonly Mock<IFriendRepository> _mockRepo;
+        private readonly Mock<IFriendsCallback> _mockCallback;
+
+        private readonly FriendsManager _manager;
+
+        public FriendsManagerTest()
+        {
+            _mockRepo = new Mock<IFriendRepository>();
+            _mockCallback = new Mock<IFriendsCallback>();
+
+            _manager = new FriendsManager(_mockRepo.Object, _mockCallback.Object);
+        }
+
+        #region SendFriendRequestAsync Tests
+
+        [Fact]
+        public async Task SendFriendRequest_Success_ReturnsSuccessAndNotifies()
+        {
+            string p1 = "Alpha";
+            string p2 = "Beta";
+            var player1 = new Player { idPlayer = 1, nickname = p1 };
+            var player2 = new Player { idPlayer = 2, nickname = p2 };
+
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(p1)).ReturnsAsync(player1);
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(p2)).ReturnsAsync(player2);
+
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(1, 2)).ReturnsAsync((FriendList)null);
+
+            _mockRepo.Setup(r => r.CreateFriendRequestAsync(1, 2))
+                .ReturnsAsync(new FriendList { idFriendList = 100, Player_idPlayer = 1, Player_idPlayer1 = 2 });
+
+            var result = await _manager.SendFriendRequestAsync(p1, p2);
+
+            Assert.Equal(FriendRequestResult.Success, result);
+
+            _mockRepo.Verify(r => r.CreateFriendRequestAsync(1, 2), Times.Once);
+        }
+
+        [Fact]
+        public async Task SendFriendRequest_SelfRequest_ReturnsCannotAddSelf()
+        {
+            var result = await _manager.SendFriendRequestAsync("Alpha", "Alpha");
+
+            Assert.Equal(FriendRequestResult.CannotAddSelf, result);
+            _mockRepo.Verify(r => r.CreateFriendRequestAsync(It.IsAny<int>(), It.IsAny<int>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task SendFriendRequest_UserNotFound_ReturnsUserNotFound()
+        {
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync("Ghost")).ReturnsAsync((Player)null);
+
+            var result = await _manager.SendFriendRequestAsync("Alpha", "Ghost");
+
+            Assert.Equal(FriendRequestResult.UserNotFound, result);
+        }
+
+        [Fact]
+        public async Task SendFriendRequest_AlreadyFriends_ReturnsAlreadyFriends()
+        {
+            var p1 = new Player { idPlayer = 1 };
+            var p2 = new Player { idPlayer = 2 };
+            var existingRel = new FriendList { friendRequest = true };
+
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync("A")).ReturnsAsync(p1);
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync("B")).ReturnsAsync(p2);
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(1, 2)).ReturnsAsync(existingRel);
+
+            var result = await _manager.SendFriendRequestAsync("A", "B");
+
+            Assert.Equal(FriendRequestResult.AlreadyFriends, result);
+        }
+
+        [Fact]
+        public async Task SendFriendRequest_RequestAlreadySent_ReturnsRequestAlreadySent()
+        {
+            var existingRel = new FriendList { friendRequest = false, Player_idPlayer = 1 };
+
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync("A")).ReturnsAsync(new Player { idPlayer = 1 });
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync("B")).ReturnsAsync(new Player { idPlayer = 2 });
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(1, 2)).ReturnsAsync(existingRel);
+
+            var result = await _manager.SendFriendRequestAsync("A", "B");
+
+            Assert.Equal(FriendRequestResult.RequestAlreadySent, result);
+        }
+
+        [Fact]
+        public async Task SendFriendRequest_DbExplodes_ReturnsFailedAndLogs()
+        {
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(It.IsAny<string>()))
+                .ThrowsAsync(new SqlExceptionBuilder().Build());
+
+            var result = await _manager.SendFriendRequestAsync("A", "B");
+
+            Assert.Equal(FriendRequestResult.Failed, result);
+        }
+
+        #endregion
+
+        #region AcceptFriendRequestAsync Tests
+
+        [Fact]
+        public async Task AcceptRequest_ValidRequest_ReturnsTrueAndUpdatesDb()
+        {
+            var requestDto = new FriendRequestData { RequesterNickname = "A", TargetNickname = "B" };
+            var p1 = new Player { idPlayer = 1 };
+            var p2 = new Player { idPlayer = 2 };
+            var pendingRel = new FriendList { idFriendList = 50, friendRequest = false };
+
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync("A")).ReturnsAsync(p1);
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync("B")).ReturnsAsync(p2);
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(1, 2)).ReturnsAsync(pendingRel);
+
+            var result = await _manager.AcceptFriendRequestAsync(requestDto);
+
+            Assert.True(result);
+            _mockRepo.Verify(r => r.AcceptFriendRequestAsync(50), Times.Once);
+        }
+
+        [Fact]
+        public async Task AcceptRequest_NoRelationshipFound_ReturnsFalse()
+        {
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(It.IsAny<string>())).ReturnsAsync(new Player());
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync((FriendList)null);
+            var result = await _manager.AcceptFriendRequestAsync(new FriendRequestData { RequesterNickname = "A", TargetNickname = "B" });
+
+            Assert.False(result);
+            _mockRepo.Verify(r => r.AcceptFriendRequestAsync(It.IsAny<int>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task AcceptRequest_DbError_ReturnsFalse()
+        {
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(It.IsAny<string>())).ThrowsAsync(new Exception("DB Boom"));
+            var result = await _manager.AcceptFriendRequestAsync(new FriendRequestData { RequesterNickname = "A", TargetNickname = "B" });
+
+            Assert.False(result);
+        }
+
+        #endregion
+
+        #region RejectFriendRequestAsync Tests
+
+        [Fact]
+        public async Task RejectRequest_Valid_RemovesEntry()
+        {
+            var requestDto = new FriendRequestData { RequesterNickname = "A", TargetNickname = "B" };
+            var rel = new FriendList { idFriendList = 99 };
+
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(It.IsAny<string>())).ReturnsAsync(new Player { idPlayer = 1 });
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(rel);
+            var result = await _manager.RejectFriendRequestAsync(requestDto);
+
+            Assert.True(result);
+            _mockRepo.Verify(r => r.RemoveFriendshipEntryAsync(99), Times.Once);
+        }
+
+        #endregion
+
+        #region RemoveFriendAsync Tests
+
+        [Fact]
+        public async Task RemoveFriend_IfFriends_RemovesEntry()
+        {
+            var requestDto = new FriendRequestData { RequesterNickname = "A", TargetNickname = "B" };
+            var rel = new FriendList { idFriendList = 77, friendRequest = true };
+
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(It.IsAny<string>())).ReturnsAsync(new Player { idPlayer = 1 });
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(rel);
+
+            var result = await _manager.RemoveFriendAsync(requestDto);
+
+            Assert.True(result);
+            _mockRepo.Verify(r => r.RemoveFriendshipEntryAsync(77), Times.Once);
+        }
+
+        [Fact]
+        public async Task RemoveFriend_IfNotFriends_ReturnsFalseAndDoesNotRemove()
+        {
+            var requestDto = new FriendRequestData { RequesterNickname = "A", TargetNickname = "B" };
+            var rel = new FriendList { idFriendList = 77, friendRequest = false };
+
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(It.IsAny<string>())).ReturnsAsync(new Player { idPlayer = 1 });
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(rel);
+
+            var result = await _manager.RemoveFriendAsync(requestDto);
+
+            Assert.False(result);
+            _mockRepo.Verify(r => r.RemoveFriendshipEntryAsync(It.IsAny<int>()), Times.Never);
+        }
+
+        #endregion
+
+        #region GetLists Tests
+
+        [Fact]
+        public async Task GetPendingRequests_ReturnsMappedList()
+        {
+            var pendingList = new List<FriendList>
+            {
+                new FriendList { idFriendList = 1, Player = new Player { nickname = "Requester1" } },
+                new FriendList { idFriendList = 2, Player = new Player { nickname = "Requester2" } }
+            };
+
+            _mockRepo.Setup(r => r.GetPendingRequestsEntitiesAsync("Target")).ReturnsAsync(pendingList);
+            var result = await _manager.GetPendingRequestsAsync("Target");
+
+            Assert.Equal(2, result.Count);
+            Assert.Equal("Requester1", result[0].RequesterNickname);
+            Assert.Equal("Target", result[0].TargetNickname);
+        }
+
+        [Fact]
+        public async Task GetFriendsList_ReturnsMappedList()
+        {
+            var friendsList = new List<Player>
+            {
+                new Player { nickname = "Friend1" }
+            };
+
+            _mockRepo.Setup(r => r.GetFriendsEntitiesAsync("Me")).ReturnsAsync(friendsList);
+
+            var result = await _manager.GetFriendsListAsync("Me");
+
+            Assert.Single(result);
+            Assert.Equal("Friend1", result[0].FriendNickname);
+            Assert.Equal("Friend", result[0].StatusMessage);
+        }
+
+        [Fact]
+        public async Task RejectRequest_DbError_ReturnsFalse()
+        {
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(It.IsAny<string>())).ReturnsAsync(new Player { idPlayer = 1 });
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ThrowsAsync(new Exception("DB Down"));
+
+            var result = await _manager.RejectFriendRequestAsync(new FriendRequestData { RequesterNickname = "A", TargetNickname = "B" });
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task RemoveFriend_DbError_ReturnsFalse()
+        {
+            _mockRepo.Setup(r => r.GetPlayerByNicknameAsync(It.IsAny<string>())).ReturnsAsync(new Player { idPlayer = 1 });
+            _mockRepo.Setup(r => r.GetFriendshipEntryAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ThrowsAsync(new Exception("DB Critical Failure"));
+
+            var result = await _manager.RemoveFriendAsync(new FriendRequestData { RequesterNickname = "A", TargetNickname = "B" });
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task SendFriendRequest_InvalidNicknames_ThrowsOrReturnsFailed()
+        {
+            string valid = "ValidUser";
+            string invalid = null;
+            var result = await _manager.SendFriendRequestAsync(valid, invalid);
+
+            Assert.Equal(FriendRequestResult.Failed, result);
+        }
+
+        [Fact]
+        public async Task GetFriendsList_Timeout_ReturnsEmptyList()
+        {
+            _mockRepo.Setup(r => r.GetFriendsEntitiesAsync(It.IsAny<string>()))
+                .ThrowsAsync(new TimeoutException("Too slow"));
+
+            var result = await _manager.GetFriendsListAsync("SlowUser");
+
+            Assert.NotNull(result);
+            Assert.Empty(result); 
+        }
+
+        #endregion
+    }
+    public class SqlExceptionBuilder
+    {
+        public SqlException Build()
+        {
+            return System.Runtime.Serialization.FormatterServices
+                .GetUninitializedObject(typeof(SqlException)) as SqlException;
+        }
+    }
+}
---
+++ UnoLisServer.Test/ValidatorsTest/FriendsValidatorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnoLisServer.Services.Validators;
+using Xunit;
+
+namespace UnoLisServer.Test
+{
+    public class FriendsValidatorTest
+    {
+        [Fact]
+        public void ValidateNicknames_BothValid_DoesNotThrow()
+        {
+            string nick1 = "AlphaUser";
+            string nick2 = "BetaUser";
+
+            FriendsValidator.ValidateNicknames(nick1, nick2);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void ValidateNicknames_InvalidRequester_ThrowsArgumentException(string invalidNick)
+        {
+            string validTarget = "ValidUser";
+
+            var ex = Assert.Throws<ArgumentException>(() =>
+                FriendsValidator.ValidateNicknames(invalidNick, validTarget));
+
+            Assert.Contains("no pueden estar vacíos", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void ValidateNicknames_InvalidTarget_ThrowsArgumentException(string invalidNick)
+        {
+            string validRequester = "ValidUser";
+
+            var ex = Assert.Throws<ArgumentException>(() =>
+                FriendsValidator.ValidateNicknames(validRequester, invalidNick));
+
+            Assert.Contains("no pueden estar vacíos", ex.Message);
+        }
+
+        [Fact]
+        public void ValidateNicknames_BothInvalid_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                FriendsValidator.ValidateNicknames("", null));
+        }
+    }
+}
