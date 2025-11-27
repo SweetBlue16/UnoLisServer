56de182389c0ed146fc9f4312913ba47ab33d0d6
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2025-11-27 09:12:44 -0600

    Added SanctionRepository and interface for sanctions
    Introduces ISanctionRepository interface and its implementation, SanctionRepository, to manage sanctions in the data layer. Provides methods to add a sanction and retrieve the active sanction for a player.

---
+++ UnoLisServer.Data/Repositories/SanctionRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using UnoLisServer.Data.RepositoryInterfaces;
+
+namespace UnoLisServer.Data.Repositories
+{
+    public class SanctionRepository : ISanctionRepository
+    {
+        private readonly Func<UNOContext> _contextFactory;
+        private readonly ISanctionRepository _sanctionRepository;
+
+        public SanctionRepository() : this(() => new UNOContext(), new SanctionRepository())
+        {
+        }
+
+        public SanctionRepository(Func<UNOContext> contextFactory, ISanctionRepository sanctionRepository)
+        {
+            _contextFactory = contextFactory;
+            _sanctionRepository = sanctionRepository ?? new SanctionRepository();
+        }
+
+        public void AddSanction(Sanction sanction)
+        {
+            using (var context = _contextFactory())
+            {
+                context.Sanction.Add(sanction);
+                context.SaveChanges();
+            }
+        }
+
+        public Sanction GetActiveSanction(int idPlayer)
+        {
+            using (var context = _contextFactory())
+            {
+                return context.Sanction
+                    .Include("Report")
+                    .Where(s => s.Player_idPlayer == idPlayer && s.sanctionEndDate > DateTime.Now)
+                    .OrderByDescending(s => s.sanctionEndDate)
+                    .FirstOrDefault();
+            }
+        }
+    }
+}
