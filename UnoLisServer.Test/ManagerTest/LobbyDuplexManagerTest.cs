f15640eacf4c8717655153d3c55bc31879b7a117
Erick <erickmelvzlpz@gmail.com>
2025-11-26 20:13:50 -0600

    Added Test for Lobby

---
+++ UnoLisServer.Test/ManagerTest/LobbyDuplexManagerTest.cs
@@ -0,0 +1,99 @@
+﻿using Moq;
+using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using UnoLisServer.Contracts.Interfaces;
+using UnoLisServer.Services;
+using UnoLisServer.Services.ManagerInterfaces;
+using Xunit;
+
+namespace UnoLisServer.Test
+{
+    public class LobbyDuplexManagerTest
+    {
+        private readonly Mock<ILobbyManager> _mockLobbyManager;
+        private readonly LobbyDuplexManager _duplexManager;
+
+        public LobbyDuplexManagerTest()
+        {
+            _mockLobbyManager = new Mock<ILobbyManager>();
+            _duplexManager = new LobbyDuplexManager(_mockLobbyManager.Object);
+        }
+
+        [Fact]
+        public void ConnectToLobby_DelegatesToManager()
+        {
+            string code = "CODE1";
+            string nick = "User1";
+
+            _duplexManager.ConnectToLobby(code, nick);
+            _mockLobbyManager.Verify(x => x.RegisterConnection(code, nick), Times.Once);
+        }
+
+        [Fact]
+        public void DisconnectFromLobby_DelegatesToManager_AndClearsState()
+        {
+            string code = "CODE1";
+            string nick = "User1";
+            _duplexManager.ConnectToLobby(code, nick); 
+            _duplexManager.DisconnectFromLobby(code, nick);
+
+            _mockLobbyManager.Verify(x => x.RemoveConnection(code, nick, It.IsAny<ILobbyDuplexCallback>()), Times.Once);
+            _duplexManager.Dispose();
+            _mockLobbyManager.Verify(x => x.RemoveConnection(code, nick, It.IsAny<ILobbyDuplexCallback>()), Times.Once); 
+        }
+
+        [Fact]
+        public void Dispose_AbruptDisconnection_TriggersCleanup()
+        {
+            string code = "CODE_ABRUPT";
+            string nick = "UserGhost";
+
+            _duplexManager.ConnectToLobby(code, nick);
+            _duplexManager.Dispose(); 
+
+            _mockLobbyManager.Verify(x => x.RemoveConnection(code, nick, It.IsAny<ILobbyDuplexCallback>()), Times.Once);
+        }
+
+        [Fact]
+        public void Dispose_CleanDisconnection_DoesNotTriggerDoubleCleanup()
+        {
+            string code = "CODE_CLEAN";
+            string nick = "GoodUser";
+
+            _duplexManager.ConnectToLobby(code, nick);
+            _duplexManager.DisconnectFromLobby(code, nick);
+            _duplexManager.Dispose();
+            _mockLobbyManager.Verify(x => x.RemoveConnection(code, nick, It.IsAny<ILobbyDuplexCallback>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task SetReadyStatus_FireAndForget_CallsManager()
+        {
+            string code = "C";
+            string nick = "U";
+            bool status = true;
+
+            _duplexManager.SetReadyStatus(code, nick, status);
+            await Task.Delay(100);
+
+            _mockLobbyManager.Verify(x => x.HandleReadyStatusAsync(code, nick, status), Times.Once);
+        }
+
+        [Fact]
+        public void CleanUp_HandlesException_DoesNotCrash()
+        {
+            string code = "ERR";
+            string nick = "User";
+            _duplexManager.ConnectToLobby(code, nick);
+
+            _mockLobbyManager.Setup(x => x.RemoveConnection(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<ILobbyDuplexCallback>()))
+                             .Throws(new Exception("Manager Crash"));
+
+            var ex = Record.Exception(() => _duplexManager.DisconnectFromLobby(code, nick));
+
+            Assert.Null(ex);
+            _mockLobbyManager.Verify(x => x.RemoveConnection(code, nick, It.IsAny<ILobbyDuplexCallback>()), Times.Once);
+        }
+    }
+}
---
+++ UnoLisServer.Test/ManagerTest/MatchmakingManagerTest.cs
@@ -0,0 +1,98 @@
+﻿using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnoLisServer.Contracts.DTOs;
+using UnoLisServer.Services;
+using UnoLisServer.Services.ManagerInterfaces;
+using Xunit;
+
+namespace UnoLisServer.Test
+{
+    public class MatchmakingManagerTest
+    {
+        private readonly Mock<ILobbyManager> _mockLobbyManager;
+        private readonly MatchmakingManager _manager;
+
+        public MatchmakingManagerTest()
+        {
+            _mockLobbyManager = new Mock<ILobbyManager>();
+            _manager = new MatchmakingManager(_mockLobbyManager.Object);
+        }
+
+        [Fact]
+        public async Task TestCreateMatchAsync_DelegatesToLobbyManager()
+        {
+            var settings = new MatchSettings();
+            var expectedResponse = new CreateMatchResponse { Success = true };
+
+            _mockLobbyManager.Setup(x => x.CreateLobbyAsync(settings))
+                             .ReturnsAsync(expectedResponse);
+
+            var result = await _manager.CreateMatchAsync(settings);
+
+            Assert.Same(expectedResponse, result);
+            _mockLobbyManager.Verify(x => x.CreateLobbyAsync(settings), Times.Once);
+        }
+
+        [Fact]
+        public async Task TestJoinMatchAsync_DelegatesToLobbyManager()
+        {
+            var expectedResponse = new JoinMatchResponse { Success = true };
+            _mockLobbyManager.Setup(x => x.JoinLobbyAsync("CODE", "User"))
+                             .ReturnsAsync(expectedResponse);
+
+            var result = await _manager.JoinMatchAsync("CODE", "User");
+
+            Assert.Same(expectedResponse, result);
+            _mockLobbyManager.Verify(x => x.JoinLobbyAsync("CODE", "User"), Times.Once);
+        }
+
+        [Fact]
+        public async Task TestSetLobbyBackgroundAsync_DelegatesToLobbyManager()
+        {
+            _mockLobbyManager.Setup(x => x.SetLobbyBackgroundAsync("CODE", "Video"))
+                             .ReturnsAsync(true);
+
+            var result = await _manager.SetLobbyBackgroundAsync("CODE", "Video");
+
+            Assert.True(result);
+            _mockLobbyManager.Verify(x => x.SetLobbyBackgroundAsync("CODE", "Video"), Times.Once);
+        }
+
+        [Fact]
+        public async Task TestSendInvitationsAsync_DelegatesToLobbyManager()
+        {
+            var list = new List<string> { "A", "B" };
+            _mockLobbyManager.Setup(x => x.SendInvitationsAsync("CODE", "Host", list))
+                             .ReturnsAsync(true);
+
+            var result = await _manager.SendInvitationsAsync("CODE", "Host", list);
+
+            Assert.True(result);
+            _mockLobbyManager.Verify(x => x.SendInvitationsAsync("CODE", "Host", list), Times.Once);
+        }
+
+        [Fact]
+        public async Task TestGetLobbySettingsAsync_ReturnsSettingsIfFound()
+        {
+            var expectedSettings = new LobbySettings { BackgroundVideoName = "Test" };
+
+            _mockLobbyManager.Setup(x => x.GetLobbySettings("CODE"))
+                             .Returns(expectedSettings);
+
+            var result = await _manager.GetLobbySettingsAsync("CODE");
+            Assert.Same(expectedSettings, result);
+        }
+
+        [Fact]
+        public async Task TestGetLobbySettingsAsync_ReturnsEmptyObjectIfNull()
+        {
+            _mockLobbyManager.Setup(x => x.GetLobbySettings("CODE"))
+                             .Returns((LobbySettings)null);
+
+            var result = await _manager.GetLobbySettingsAsync("CODE");
+            Assert.NotNull(result);
+            Assert.Null(result.BackgroundVideoName); 
+        }
+    }
+}
