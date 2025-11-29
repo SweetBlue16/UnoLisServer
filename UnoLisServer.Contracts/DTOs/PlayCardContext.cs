fc9d0bdfc97030c0b64c3e3c49f8a431ee556ebd
Erick <erickmelvzlpz@gmail.com>
2025-11-29 02:53:21 -0600

    Added DTO PlayerDataContext to improve parameters number

---
+++ UnoLisServer.Contracts/DTOs/PlayCardContext.cs
@@ -0,0 +1,20 @@
+﻿using UnoLisServer.Contracts.DTOs;
+
+namespace UnoLisServer.Contracts.DTOs // <--- Faltaba esto
+{
+    public class PlayCardContext
+    {
+        public string LobbyCode { get; set; }
+        public string Nickname { get; set; }
+        public string CardId { get; set; }
+        public int? SelectedColorId { get; set; }
+
+        public PlayCardContext(string lobbyCode, string nickname, string cardId, int? colorId)
+        {
+            LobbyCode = lobbyCode;
+            Nickname = nickname;
+            CardId = cardId;
+            SelectedColorId = colorId;
+        }
+    }
+}
--- UnoLisServer.Contracts/DTOs/ResultData.cs
+++ UnoLisServer.Contracts/DTOs/ResultData.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace UnoLisServer.Contracts.DTOs
 {
+    [DataContract]
     public class ResultData
     {
+        [DataMember]
         public string Nickname { get; set; }
+
+        [DataMember]
         public int Score { get; set; }
+
+        [DataMember]
         public int Position { get; set; }
+
+        [DataMember]
         public bool IsWinner { get; set; }
     }
 }
